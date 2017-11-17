1a3e5b70c750150b342036b1c3e071c5ae2a290d
jmayorga <jmayorgadev@gmail.com>
2017-11-17 09:23:13 -0500

    Slider and Princing examples added

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,21 @@ let autoprefixer = require('gulp-autoprefixer');
 
 
 gulp.task('styles', function(){
-    gulp.src('./styles/style.css')
+    gulp.src('./styles/style_mobile.css')
+    .pipe(autoprefixer())
+    .pipe(gulp.dest('build'));
+
+    gulp.src('./styles/style_slider.css')
+    .pipe(autoprefixer())
+    .pipe(gulp.dest('build'))
+
+    gulp.src('./styles/style_pricing.css')
     .pipe(autoprefixer())
     .pipe(gulp.dest('build'))
 });
 
 gulp.task('watch', function(){
-    gulp.watch("./styles/style.css",['styles'])
+    gulp.watch("./styles/style_mobile.css",['styles']);
+    gulp.watch("./styles/style_slider.css", ["styles"]);
+    gulp.watch("./styles/style_pricing.css", ["styles"]);
 });
