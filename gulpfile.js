b425e4322a7694c76194ea6a71e73e734d71d0f9
jmayorga <jmayorgadev@gmail.com>
2017-11-17 13:45:17 -0500

    Mobile app added

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,37 @@
-let gulp = require('gulp');
-let autoprefixer = require('gulp-autoprefixer');
+let gulp = require("gulp");
+let autoprefixer = require("gulp-autoprefixer");
 
-
-gulp.task('styles', function(){
-    gulp.src('./styles/style_mobile.css')
+gulp.task("styles", function() {
+  gulp
+    .src("./styles/style_mobile.css")
     .pipe(autoprefixer())
-    .pipe(gulp.dest('build'));
+    .pipe(gulp.dest("build"));
 
-    gulp.src('./styles/style_slider.css')
+  gulp
+    .src("./styles/style_slider.css")
     .pipe(autoprefixer())
-    .pipe(gulp.dest('build'))
+    .pipe(gulp.dest("build"));
 
-    gulp.src('./styles/style_pricing.css')
+  gulp
+    .src("./styles/style_pricing.css")
     .pipe(autoprefixer())
-    .pipe(gulp.dest('build'))
+    .pipe(gulp.dest("build"));
+
+  gulp
+    .src("./styles/style_form.css")
+    .pipe(autoprefixer())
+    .pipe(gulp.dest("build"));
+
+  gulp
+    .src("./styles/style_app.css")
+    .pipe(autoprefixer())
+    .pipe(gulp.dest("build"));
 });
 
-gulp.task('watch', function(){
-    gulp.watch("./styles/style_mobile.css",['styles']);
-    gulp.watch("./styles/style_slider.css", ["styles"]);
-    gulp.watch("./styles/style_pricing.css", ["styles"]);
+gulp.task("watch", function() {
+  gulp.watch("./styles/style_mobile.css", ["styles"]);
+  gulp.watch("./styles/style_slider.css", ["styles"]);
+  gulp.watch("./styles/style_pricing.css", ["styles"]);
+  gulp.watch("./styles/style_form.css", ["styles"]);
+  gulp.watch("./styles/style_app.css", ["styles"]);
 });
